90417adc9c2b297a4363e49b5809f4098e64f2d5
Leyder Sanchez <lysanchez02@misena.edu.co>
2023-04-01 12:30:19 -0500

    android first commit

---
+++ src/main/java/com/leyder/reciclaapp/Menu_App.java
@@ -0,0 +1,14 @@
+package com.leyder.reciclaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_app);
+    }
+}
---
+++ src/main/java/com/leyder/reciclaapp/recicla_ya.java
@@ -0,0 +1,14 @@
+package com.leyder.reciclaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recicla_ya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recicla_ya);
+    }
+}
